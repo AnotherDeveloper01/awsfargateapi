8db1dd48525177e2c216e2c26413d5e5ce5a244d
sxg0csn <sxg0csn@LT321886>
2020-04-29 19:35:40 -0400

    Test checkin

--- ecsfargatecoreapi/Controllers/WeatherForecastController.cs
+++ ecsfargatecoreapi/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace ecsfargatecoreapi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "EndaManding", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
